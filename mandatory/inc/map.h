e0b1992fa1ec220cdc46d16c9af8e05179c00d12
Furkan Güryel <fguryel@k1m18s03.42istanbul.com.tr>
2026-02-04 19:54:05 +0300

    file name parsing available to test

--- mandatory/inc/map.h
+++ mandatory/inc/map.h
@@ -1,19 +1,22 @@
 #ifndef MAP_H
-#define MAP_H
+# define MAP_H
 
-// Define error flags
-#define ERR_NULL_FILENAME    (1 << 0)
-#define ERR_EMPTY_STRING     (1 << 1)
-#define ERR_TOO_SHORT        (1 << 2)
-#define ERR_NO_EXTENSION     (1 << 3)
-#define ERR_WRONG_EXTENSION  (1 << 4)
-#define ERR_ONLY_EXTENSION   (1 << 5)
-#define ERR_EXT_IN_MIDDLE    (1 << 6)
-#define ERR_CASE_SENSITIVE   (1 << 7)
-#define ERR_TRAILING_SPACE   (1 << 8)
-#define ERR_DIR_AS_FILE      (1 << 9)
-#define ERR_FILE_NOT_FOUND   (1 << 10)
-#define ERR_PERMISSION       (1 << 11)
+# define _GNU_SOURCE
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdint.h>
+
+# define ERR_NULL_FILENAME    (1 << 0)
+# define ERR_EMPTY_STRING     (1 << 1)
+# define ERR_TOO_SHORT        (1 << 2)
+# define ERR_WRONG_EXTENSION  (1 << 3)
+# define ERR_ONLY_EXTENSION   (1 << 4)
+# define ERR_CASE_SENSITIVE   (1 << 5)
+# define ERR_TRAILING_SPACE   (1 << 6)
+# define ERR_IS_DIRECTORY     (1 << 7)
+# define ERR_OPEN_FAILED      (1 << 8)
+
+# include "libft.h"
 
 typedef struct s_map
 {
@@ -23,6 +26,8 @@ typedef struct s_map
 	char	*no_path;
 	char	*so_path;
 }			t_map;
+
+int	parse_file(const char *file_path);
 
 #endif
 
--- mandatory/src/parsing/parse_file.c
+++ mandatory/src/parsing/parse_file.c
@@ -1,12 +1,90 @@
-#include <unistd.h>
+#include "map.h"
 
-int parse_file(const char *file_path)
+static int	check_extension(const char *path, int len)
 {
-    int path_len;
-    uint16_t error;
+	const char	*ext;
+	int			i;
 
-    error = 0;
-    path_len = ft_strlen(file_path);
+	ext = path + len - 4;
+	if (ft_strncmp(ext, ".cub", 4) == 0)
+		return (0);
+	i = 0;
+	while (i < 4)
+	{
+		if (ft_tolower(ext[i]) != ".cub"[i])
+			return (ERR_WRONG_EXTENSION);
+		i++;
+	}
+	return (ERR_CASE_SENSITIVE);
+}
 
-    
+static int	validate_filename(const char *path)
+{
+	int	len;
+
+	if (!path)
+		return (ERR_NULL_FILENAME);
+	if (!*path)
+		return (ERR_EMPTY_STRING);
+	len = ft_strlen(path);
+	if (len < 5)
+		return (ERR_TOO_SHORT);
+	if (path[len - 1] == ' ')
+		return (ERR_TRAILING_SPACE);
+	return (check_extension(path, len));
+}
+
+static int	validate_file_access(const char *path)
+{
+	int	fd;
+
+	fd = open(path, O_DIRECTORY);
+	if (fd >= 0)
+	{
+		close(fd);
+		return (ERR_IS_DIRECTORY);
+	}
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (ERR_OPEN_FAILED);
+	close(fd);
+	return (0);
+}
+
+static void	print_error(int error)
+{
+	const char	*msg;
+
+	if (error == ERR_NULL_FILENAME)
+		msg = "Error: NULL filename\n";
+	else if (error == ERR_EMPTY_STRING)
+		msg = "Error: Empty filename\n";
+	else if (error == ERR_TOO_SHORT)
+		msg = "Error: Filename too short\n";
+	else if (error == ERR_TRAILING_SPACE)
+		msg = "Error: Filename has trailing spaces\n";
+	else if (error == ERR_CASE_SENSITIVE)
+		msg = "Error: Extension must be lowercase .cub\n";
+	else if (error == ERR_WRONG_EXTENSION)
+		msg = "Error: Wrong extension (must be .cub)\n";
+	else if (error == ERR_IS_DIRECTORY)
+		msg = "Error: Path is a directory\n";
+	else
+		msg = "Error: Cannot open file\n";
+	write(2, msg, ft_strlen(msg));
+}
+
+int	parse_file(const char *file_path)
+{
+	int	error;
+
+	error = validate_filename(file_path);
+	if (!error)
+		error = validate_file_access(file_path);
+	if (error)
+	{
+		print_error(error);
+		return (1);
+	}
+	return (0);
 }
