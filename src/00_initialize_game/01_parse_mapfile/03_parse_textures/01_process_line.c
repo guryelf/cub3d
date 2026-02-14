e07c542ada29c8042baf580cd7bf62ab39252722
Rıdvan Akman <dr.ridvanakman@gmail.com>
2026-02-14 21:22:32 +0300

    map olayı

--- src/00_initialize_game/01_parse_mapfile/03_parse_textures/01_process_line.c
+++ src/00_initialize_game/01_parse_mapfile/03_parse_textures/01_process_line.c
@@ -6,64 +6,11 @@
 /*   By: rakman <rakman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 17:40:00 by rakman            #+#    #+#             */
-/*   Updated: 2026/02/14 17:40:00 by rakman           ###   ########.fr       */
+/*   Updated: 2026/02/14 19:40:00 by rakman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
-
-static int	validate_color_range(t_texture *tex)
-{
-	if (tex->r < 0 || tex->r > 255)
-		return (TEX_ERR_COLOR_RANGE);
-	if (tex->g < 0 || tex->g > 255)
-		return (TEX_ERR_COLOR_RANGE);
-	if (tex->b < 0 || tex->b > 255)
-		return (TEX_ERR_COLOR_RANGE);
-	return (0);
-}
-
-static int	parse_rgb_values(char **split, t_texture *tex)
-{
-	int	i;
-
-	tex->r = ft_atoi(split[0]);
-	tex->g = ft_atoi(split[1]);
-	tex->b = ft_atoi(split[2]);
-	i = 0;
-	while (i < 3)
-		free(split[i++]);
-	free(split);
-	if (validate_color_range(tex))
-		return (TEX_ERR_COLOR_RANGE);
-	tex->set = 1;
-	return (0);
-}
-
-int	parse_color_values(char *line, t_texture *tex)
-{
-	char	**split;
-	int		i;
-
-	if (tex->set)
-		return (TEX_ERR_DUPLICATE);
-	while (*line && (*line == ' ' || *line == '\t'))
-		line++;
-	split = ft_split(line, ',');
-	if (!split)
-		return (TEX_ERR_INVALID_COLOR);
-	i = 0;
-	while (split[i])
-		i++;
-	if (i != 3)
-	{
-		while (--i >= 0)
-			free(split[i]);
-		free(split);
-		return (TEX_ERR_INVALID_COLOR);
-	}
-	return (parse_rgb_values(split, tex));
-}
 
 int	get_texture_type(char *line)
 {
@@ -103,3 +50,4 @@ int	process_line(char *line, t_map *map)
 		return (parse_color_values(line + 2, &map->floor));
 	return (parse_color_values(line + 2, &map->ceil));
 }
+
---
+++ src/00_initialize_game/01_parse_mapfile/03_parse_textures/03_parse_color.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   03_parse_color.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rakman <rakman@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 19:40:00 by rakman            #+#    #+#             */
+/*   Updated: 2026/02/14 19:40:00 by rakman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "map.h"
+
+static int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	validate_color_range(t_texture *tex)
+{
+	if (tex->r < 0 || tex->r > 255)
+		return (TEX_ERR_COLOR_RANGE);
+	if (tex->g < 0 || tex->g > 255)
+		return (TEX_ERR_COLOR_RANGE);
+	if (tex->b < 0 || tex->b > 255)
+		return (TEX_ERR_COLOR_RANGE);
+	return (0);
+}
+
+static int	parse_rgb_values(char **split, t_texture *tex)
+{
+	int	i;
+
+	if (!is_valid_number(split[0]) || !is_valid_number(split[1])
+		|| !is_valid_number(split[2]))
+	{
+		i = 0;
+		while (i < 3)
+			free(split[i++]);
+		free(split);
+		return (TEX_ERR_INVALID_COLOR);
+	}
+	tex->r = ft_atoi(split[0]);
+	tex->g = ft_atoi(split[1]);
+	tex->b = ft_atoi(split[2]);
+	i = 0;
+	while (i < 3)
+		free(split[i++]);
+	free(split);
+	if (validate_color_range(tex))
+		return (TEX_ERR_COLOR_RANGE);
+	tex->set = 1;
+	return (0);
+}
+
+int	parse_color_values(char *line, t_texture *tex)
+{
+	char	**split;
+	int		i;
+
+	if (tex->set)
+		return (TEX_ERR_DUPLICATE);
+	while (*line && (*line == ' ' || *line == '\t'))
+		line++;
+	split = ft_split(line, ',');
+	if (!split)
+		return (TEX_ERR_INVALID_COLOR);
+	i = 0;
+	while (split[i])
+		i++;
+	if (i != 3)
+	{
+		while (--i >= 0)
+			free(split[i]);
+		free(split);
+		return (TEX_ERR_INVALID_COLOR);
+	}
+	return (parse_rgb_values(split, tex));
+}
