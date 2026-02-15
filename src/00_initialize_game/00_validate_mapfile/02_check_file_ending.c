91fd806324f79660dab84976e6421d63eeca5e27
Rıdvan Akman <dr.ridvanakman@gmail.com>
2026-02-15 05:08:16 +0300

    son

--- src/00_initialize_game/00_validate_mapfile/02_check_file_ending.c
+++ src/00_initialize_game/00_validate_mapfile/02_check_file_ending.c
@@ -6,59 +6,50 @@
 /*   By: rakman <rakman@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 00:00:00 by rakman            #+#    #+#             */
-/*   Updated: 2026/02/15 04:56:04 by rakman           ###   ########.fr       */
+/*   Updated: 2026/02/15 05:08:03 by rakman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/error.h"
+#include "../../../inc/get_next_line.h"
+#include "../../../inc/libft.h"
 #include <fcntl.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-static int	get_file_size(int fd)
+static char	*get_last_line(int fd)
 {
-	int		size;
-	char	c;
+	char	*line;
+	char	*last_line;
 
-	size = 0;
-	while (read(fd, &c, 1) > 0)
-		size++;
-	return (size);
+	last_line = NULL;
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (last_line)
+			free(last_line);
+		last_line = line;
+		line = get_next_line(fd);
+	}
+	return (last_line);
 }
 
 int	file_ends_with_next_line(const char *file_path)
 {
 	int		fd;
-	int		size;
-	char	last_char;
+	char	*last_line;
+	int		result;
 
 	fd = open(file_path, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	size = get_file_size(fd);
+	last_line = get_last_line(fd);
 	close(fd);
-	if (size == 0)
-		return (0);
-	fd = open(file_path, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	lseek(fd, size - 1, SEEK_SET);
-	if (read(fd, &last_char, 1) <= 0)
-	{
-		close(fd);
-		return (0);
-	}
-	close(fd);
-	if (last_char == '\n')
-		return (1);
-	return (0);
-}
-
-int	validate_file_ending(const char *file_path)
-{
-	if (file_ends_with_next_line(file_path))
-	{
-		write(2, "Error: File ends with a newline\n", 33);
-		return (1);
-	}
-	return (0);
+	result = 0;
+	if (last_line && ft_strlen(last_line) > 0
+		&& last_line[ft_strlen(last_line) - 1] == '\n')
+		result = 1;
+	if (last_line)
+		free(last_line);
+	return (result);
 }
